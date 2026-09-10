e7b9f79ef802328f39619f3269be10592d626271
Ronnie <ronnieroymwangi.com>
2026-09-10 13:07:38 +0300

    Thu Sep 10 01:07:38 PM EAT 2026

--- src/main/java/com/ronnie/digifarm/models/WeightLogs.java
+++ src/main/java/com/ronnie/digifarm/models/WeightLogs.java
@@ -4,6 +4,7 @@ package com.ronnie.digifarm.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
