13dc0ee04b931fe15435aaef45a0563d34640f2e
Ronnie <ronnieroymwangi.com>
2026-09-10 05:30:42 +0300

    Thu Sep 10 05:30:42 AM EAT 2026

--- src/main/java/com/ronnie/digifarm/models/InventoryItems.java
+++ src/main/java/com/ronnie/digifarm/models/InventoryItems.java
@@ -1,14 +1,10 @@
 package com.ronnie.digifarm.models;
 
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Getter
